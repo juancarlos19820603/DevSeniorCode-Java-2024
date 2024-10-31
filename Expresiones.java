e3f2f30a915c139864ce4743657b9d2bc7e912e3
Juan Carlos Londoño <juancarloslondono1982@gmail.com>
2024-10-30 22:21:28 -0500

    "Updated subproject commit hash to indicate dirty state"

---
+++ Expresiones.java
@@ -0,0 +1,23 @@
+public class Expresiones {
+
+    public static void main(String[] args) {
+        var num1 =10;
+        var num2 = -5;
+        var num3 = num1+num2;
+        var num4 = 40 - num3;
+        System.out.println(num4);     
+        
+        
+        //+
+//-
+//*
+// /
+var num5 =10 * 25 - 30 / 8;
+System.out.println(num5);
+
+var num6 = 157/11;
+var num7 = 157%11;
+System.out.printf("la dvision entre 157 y 11 es %d y su residuo es %d%n",num6, num7);
+  }
+} 
+
